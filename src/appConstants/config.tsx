e4c320ffecb554c0d8527a5f4501259f691b4771
Sujan STC <sujandstc@gmail.com>
2023-04-21 17:34:14 +0545

    app

--- src/appConstants/config.tsx
+++ src/appConstants/config.tsx
@@ -1,4 +1,4 @@
 export const axios_base_url =
   process.env.NODE_ENV === "production"
-    ? "https://candid-flan-5e0884.netlify.app/"
+    ? "https://visionary-croquembouche-894e5a.netlify.app/"
     : "http://192.168.101.7:3000/";
